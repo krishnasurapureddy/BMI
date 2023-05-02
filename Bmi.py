37562cea959c49db7741ab27271eb0358a063a73
krishna <sgskprasad2000@gmail.com>
2023-05-02 20:14:00 +0530

    add bmi file

--- Bmi.py
+++ Bmi.py
@@ -1,5 +1,5 @@
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in feet: "))
+weight = float(input("Enter your weight in pounds: "))
 
 bmi = weight / (height ** 2)
 
